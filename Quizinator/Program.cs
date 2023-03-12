d97b79e2888307ffa6edfb4eb4ea4f0de7a6c672
Ваня <vanya.alekseev6@gmail.com>
2023-03-13 01:26:37 +0300

    Removed default comments, added my own comment and removed my 'debug'

--- Quizinator/Program.cs
+++ Quizinator/Program.cs
@@ -9,25 +9,16 @@ namespace Quizinator;
 
 class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args)
     {
-        try
-        {
-            Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
-            BuildAvaloniaApp()
-                .StartWithClassicDesktopLifetime(args);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-        }
+        // Honestly, I still have no idea why registration is this early? But it works! works fine at least!
+        Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
+        
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
     }
-
-    // Avalonia configuration, don't remove; also used by visual designer.
+    
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
